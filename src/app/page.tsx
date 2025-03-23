036f62ebb101279d98a58b06ea00627276483c39
Yousif <yousif.khalil.abdulkarim.1999@gmail.com>
2025-03-23 01:09:13 +0100

    Removed Hero ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,3 @@
-import "@/globals.css";
-import { Button } from "@heroui/button";
-
 export default function Home() {
-    return (
-        <div>
-            <Button> asdasd</Button>
-        </div>
-    );
+    return <div>asdasd</div>;
 }
